fc6095155238353fbb6d650c9dd217d9859dac29
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-01 16:25:20 +0300

    created the pint light and used useGLTF

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,5 +1,16 @@
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 function Computers() {
-  return <div>Computers</div>;
+  const computer = useGLTF("public/desktop_pc/scene.gltf");
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.25} groundColor="black" />
+      <pintLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
 }
 
 export default Computers;
