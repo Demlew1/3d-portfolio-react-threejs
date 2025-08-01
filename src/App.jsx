9705b7197cba0cb7b809aae00f3a979345e1e053
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-01 10:07:42 +0300

    finished the component's flow in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
 import { About, Contact, Hero, Navbar, Tech, StarCanvas } from "./components";
+import { BrowserRouter } from "react-router-dom";
 export default function App() {
-  return <div>My_3D_Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Tech />
+        <div className="relative z-0">
+          <Contact />
+          <StarCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
