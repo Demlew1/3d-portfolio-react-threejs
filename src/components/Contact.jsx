ba6d93d8c4d3b9f676e5a6a795fe43fc92d93532
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-03 13:54:51 +0300

    started working on the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,18 @@
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { slideIn } from "../utils/motion";
+
 function Contact() {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [Loading, setLoading] = useState(false);
   return <div>contact us</div>;
 }
 
