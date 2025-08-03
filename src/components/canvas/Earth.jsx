8600ed919e4cf11015b27301d722f8b6f94b1176
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-03 16:31:04 +0300

    working on the 3D part

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,3 +1,19 @@
-export default function EarthCanvas() {
+import { Suspense } from "react";
+import { Canvas } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/fiber";
+import CanvasLoader from "../Loader";
+export default function Earth() {
   return <div>Earth</div>;
+}
+function EarthCanvas() {
+  return (
+    <Canvas
+      shadows
+      frameLoop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader/>}
+    </Canvas>
+  );
 }
