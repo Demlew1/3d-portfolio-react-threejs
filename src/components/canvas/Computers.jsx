f033023bbbf57cd93443fcb90f75d558e6f299b0
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-08-06 11:18:39 +0300

    adjusted some positioning and tried to fix mobile scrolling problem

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,13 +6,16 @@ import CanvasLoader from "../Loader";
 function Computers({ isMobile }) {
   const computer = useGLTF("/desktop_pc/scene.gltf");
   const computerRef = useRef();
+
   useFrame((state) => {
     if (computerRef.current) {
       const elapsed = state.clock.getElapsedTime();
       computerRef.current.position.y = Math.sin(elapsed * 1.5) * 0.1 - 1.5;
     }
   });
+
   if (!computer?.scene) return null;
+
   return (
     <group>
       <hemisphereLight intensity={1.2} groundColor="black" />
@@ -28,17 +31,19 @@ function Computers({ isMobile }) {
       <primitive
         ref={computerRef}
         object={computer.scene}
-        scale={isMobile ? 2.5 : 5}
-        position={isMobile ? [0, -1.5, 1] : [1, -1, -1]}
-        rotation={[-0.05, -0.4, -0.05]}
+        scale={isMobile ? 2.5 : 6}
+        position={isMobile ? [0, -1.5, 1] : [0, -1.2, 2]}
+        rotation={[0, Math.PI, 0]}
         castShadow
         receiveShadow
       />
     </group>
   );
 }
-const ComputerCanvas = () => {
+
+export default function ComputerCanvas() {
   const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 600px)");
     setIsMobile(mediaQuery.matches);
@@ -51,11 +56,17 @@ const ComputerCanvas = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen">
+    <div
+      className="w-full h-screen touch-none"
+      onTouchMove={(e) => e.stopPropagation()} // Smooth scrolling
+    >
       <Canvas
-        frameloop="always"
         shadows
-        camera={{ position: isMobile ? [0, 5, 12] : [8, 0, 12], fov: 28 }}
+        frameloop="always"
+        camera={{
+          position: isMobile ? [0, 5, 12] : [-16, 8, 6],
+          fov: 30,
+        }}
         gl={{
           preserveDrawingBuffer: true,
           antialias: true,
@@ -68,8 +79,9 @@ const ComputerCanvas = () => {
             enableZoom={false}
             enablePan={false}
             enableRotate={!isMobile}
-            maxPolarAngle={Math.PI / 2.2}
+            maxPolarAngle={Math.PI / 2.1}
             minPolarAngle={Math.PI / 3}
+            autoRotate={false}
           />
           <Computers isMobile={isMobile} />
         </Suspense>
@@ -77,5 +89,4 @@ const ComputerCanvas = () => {
       </Canvas>
     </div>
   );
-};
-export default ComputerCanvas;
+}
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -2,12 +2,14 @@ import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+
 function Earth() {
   const earth = useGLTF("/planet/scene.gltf");
   return (
     <primitive object={earth.scene} scale={5} position-y={0} rotation-y={0} />
   );
 }
+
 export default function EarthCanvas() {
   const [isMobile, setIsMobile] = useState(false);
 
@@ -18,25 +20,30 @@ export default function EarthCanvas() {
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
+
     mediaQuery.addEventListener("change", handleMediaQueryChange);
     return () =>
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
   }, []);
+
   return (
-    <div className="w-full h-full">
+    <div
+      className="w-full h-screen touch-none"
+      onTouchMove={(e) => e.stopPropagation()} // Smooth scrolling
+    >
       <Canvas
         shadows
-        frameLoop="demand"
+        frameloop="demand"
         gl={{ preserveDrawingBuffer: true }}
-        camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+        camera={{ fov: 45, near: 0.1, far: 200, position: [0, 2, 6] }}
       >
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls
-            autoRotate
             enableZoom={false}
             enableRotate={!isMobile}
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
+            autoRotate={!isMobile}
           />
           <Earth />
         </Suspense>
